835bc64ab4d92e0eef05133d30c6829768d73b31
mr-orange-fiber <roman.sascha@googlemail.com>
2021-02-02 14:28:13 +0100

    set up eval script for test run

--- run_sac.sh
+++ run_sac.sh
@@ -3,11 +3,17 @@ cd eval
 python sac_learn.py \
   --env Pendulum-v0 \
   --name sac_pendulum \
-  --epochs 1 \
-  --repeat 2
+  --epochs 10 \
+  --repeat 3
 
 python sac_learn.py \
   --env Ant-v2 \
   --name sac_ant \
-  --epochs 1 \
-  --repeat 2
+  --epochs 20 \
+  --repeat 3
+
+python sac_learn.py \
+  --env HalfCheetah-v2 \
+  --name sac_ant \
+  --epochs 20 \
+  --repeat 3

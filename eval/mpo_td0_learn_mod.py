d076ec87f5f3aec5febdbe36837185aa3f1910e6
mr-orange-fiber <roman.sascha@googlemail.com>
2021-02-09 18:14:22 +0100

    adding config

--- eval/mpo_td0_learn_mod.py
+++ eval/mpo_td0_learn_mod.py
@@ -46,5 +46,6 @@ if __name__ == '__main__':
             update_steps=args.update_steps,
             update_after=args.update_after,
             min_steps_per_epoch=args.min_steps_per_epoch,
+            test_after=args.test_after
         )()
 

5ff55a0da2000f02fd3438b808b03175de4fee25
mr-orange-fiber <roman.sascha@googlemail.com>
2021-02-20 19:20:48 +0100

    changing defaults

--- run_mpo_td.sh
+++ run_mpo_td.sh
@@ -1,5 +1,7 @@
 pip install -e
 cd eval
+# parser.add_argument('--eps_mean', type=float, default=0.001)
+# parser.add_argument('--eps_cov', type=float, default=0.000001)
 
 python mpo_td0_learn_mod_sac_update.py \
   --env HalfCheetah-v2 \
@@ -9,14 +11,7 @@ python mpo_td0_learn_mod_sac_update.py \
   --min_steps_per_epoch 50 \
   --test_after 4000 \
   --update_steps 50 \
-  --update_after 50
+  --update_after 50 \
+  --eps_mean 0.001 \
+  --eps_cov 0.00001 \
 
-python mpo_td0_learn_mod_sac_update.py \
-  --env HalfCheetah-v2 \
-  --name mpo_td0_mod_q_polyak_sac_update_100 \
-  --repeat 3 \
-  --total_steps 160000 \
-  --min_steps_per_epoch 50 \
-  --test_after 4000 \
-  --update_steps 100 \
-  --update_after 100

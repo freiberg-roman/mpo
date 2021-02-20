41387e713c40c85f0032eb177fd6ba7e596d3235
mr-orange-fiber <roman.sascha@googlemail.com>
2021-02-20 20:22:28 +0100

    test suite for low eps values

--- run_mpo_td.sh
+++ run_mpo_td.sh
@@ -24,3 +24,27 @@ python mpo_td0_learn_mod_sac_update.py \
   --update_after 500 \
   --eps_mean 0.0005 \
   --eps_cov 0.00001 \
+
+python mpo_td0_learn_mod_sac_update.py \
+  --env HalfCheetah-v2 \
+  --name mpo_td0_hch_250_005_0001 \
+  --repeat 2 \
+  --total_steps 60000 \
+  --min_steps_per_epoch 100 \
+  --test_after 4000 \
+  --update_steps 250 \
+  --update_after 250 \
+  --eps_mean 0.005 \
+  --eps_cov 0.0001 \
+
+python mpo_td0_learn_mod_sac_update.py \
+  --env HalfCheetah-v2 \
+  --name mpo_td0_hch_500_005_0001 \
+  --repeat 2 \
+  --total_steps 60000 \
+  --min_steps_per_epoch 100 \
+  --test_after 4000 \
+  --update_steps 500 \
+  --update_after 500 \
+  --eps_mean 0.005 \
+  --eps_cov 0.0001 \
